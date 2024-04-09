e71cf297f633b146461a7a37b32c904925c0d8bc
HJH201314 <1979396625@qq.com>
2024-04-10 01:40:30 +0800

    refactor: monorepo

---
+++ admin/vite.config.ts
@@ -0,0 +1,54 @@
+import { ConfigEnv, UserConfig, loadEnv } from 'vite';
+import { viteMockServe } from 'vite-plugin-mock';
+import createVuePlugin from '@vitejs/plugin-vue';
+import vueJsx from '@vitejs/plugin-vue-jsx';
+import svgLoader from 'vite-svg-loader';
+import VueDevTools from 'vite-plugin-vue-devtools';
+
+import path from 'path';
+
+const CWD = process.cwd();
+
+// https://vitejs.dev/config/
+export default ({ mode }: ConfigEnv): UserConfig => {
+  const { VITE_BASE_URL } = loadEnv(mode, CWD);
+  return {
+    base: VITE_BASE_URL,
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, './src'),
+      },
+    },
+
+    css: {
+      preprocessorOptions: {
+        less: {
+          modifyVars: {
+            hack: `true; @import (reference) "${path.resolve('src/style/variables.less')}";`,
+          },
+          math: 'strict',
+          javascriptEnabled: true,
+        },
+      },
+    },
+
+    plugins: [
+      createVuePlugin(),
+      vueJsx(),
+      viteMockServe({
+        mockPath: 'mock',
+        localEnabled: true,
+      }),
+      svgLoader(),
+      VueDevTools(),
+    ],
+
+    server: {
+      port: 5174,
+      host: '0.0.0.0',
+      proxy: {
+        '/api': 'http://127.0.0.1:3000/',
+      },
+    },
+  };
+};
