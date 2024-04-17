1fc3d299a1ee2e475d2372134f5fde07442e731a
HJH201314 <1979396625@qq.com>
2024-04-18 00:09:34 +0800

    fix: rm tdesign

--- client/src/main.ts
+++ client/src/main.ts
@@ -6,7 +6,6 @@ import { install } from '@icon-park/vue-next/es/all';
 import App from './App.vue';
 import router from './router';
 import 'ant-design-vue/dist/reset.css';
-import 'tdesign-vue-next/es/style/index.css';
 import '@icon-park/vue-next/styles/index.css';
 import '@/assets/main.scss';
 
